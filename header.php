b6d2d772252e38998c06ff91701ee85127b90284
alyviacasey <lesliealyviacasey@gmail.com>
2024-09-18 22:52:32 -0400

    Changes to be committed: 	modified:   css/style.css 	modified:   header.php

--- header.php
+++ header.php
@@ -3,25 +3,26 @@
         <h1>Banner Image</h1>
     </div>
     <div class = "nav">
-        <a href = "index.php">Home</a>
-        <a href = "">Carnival</a>
-        <a href = "">Shop</a>
-        <?php
-            if(isset($_SESSION["userid"]))
-            {
-        ?>
-            <a href = "profile.php"><?php echo $_SESSION["username"]; ?></a>
-            <a href = "includes/logout.inc.php">LOGOUT</a>
-        <?php
-            }
-            else
-            {
-        ?>
-            <a href = "signup.php">SIGN UP</a>
-            <a href = "signup.php">LOGIN</a>
-        <?php
-            }
-        ?>
+        <ul>
+            <a href = "index.php">Home</a>
+            <a href = "">Carnival</a>
+            <a href = "">Shop</a>
+            <?php
+                if(isset($_SESSION["userid"]))
+                {
+            ?>
+                <a href = "profile.php"><?php echo $_SESSION["username"]; ?></a>
+                <a href = "includes/logout.inc.php">LOGOUT</a>
+            <?php
+                }
+                else
+                {
+            ?>
+                <a href = "signup.php">SIGN UP</a>
+                <a href = "signup.php">LOGIN</a>
+            <?php
+                }
+            ?>
         </ul>
     </div>
 </header>
