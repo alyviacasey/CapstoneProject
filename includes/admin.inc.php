708e335e01153ad0223e657a2b2bd30db3743f14
alyviacasey <lesliealyviacasey@gmail.com>
2024-10-07 19:28:59 -0400

    Changes to be committed: 	modified:   includes/admin.inc.php

--- includes/admin.inc.php
+++ includes/admin.inc.php
@@ -4,7 +4,7 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // SET VARIABLES from form input
     $theme = htmlspecialchars($_POST["theme"], ENT_QUOTES, 'UTF-8');
-    $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
+    $name = htmlspecialchars($_POST["model"], ENT_QUOTES, 'UTF-8');
     $material = htmlspecialchars($_POST["material"], ENT_QUOTES, 'UTF-8');
 
     // INSTANTIATE MODEL CONTROLLER ( create new model )
@@ -13,7 +13,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     include "../classes/model.classes.php";
     include "../classes/model-contr.classes.php";
 
-    $signup = new ModelContr($theme, $name, $material);
+    $create = new ModelContr($theme, $name, $material);
 
     // Running error handlers and user signup
 
