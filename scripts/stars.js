35a248d6577d9436fc6ca03c86db008f0de9df5a
alyviacasey <lesliealyviacasey@gmail.com>
2024-10-09 17:17:49 -0400

    Changes to be committed: 	modified:   css/style.css 	modified:   scripts/stars.js

--- scripts/stars.js
+++ scripts/stars.js
@@ -2,7 +2,6 @@ const stars = document.querySelectorAll(".stars i");
 
 stars.forEach((star, index1) => {
     star.addEventListener("click", () => {
-        console.log(index1);
         stars.forEach((star, index2) => {
             index1 >= index2 ? star.classList.add("checked") : star.classList.remove("checked");
         });
