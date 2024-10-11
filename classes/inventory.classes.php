445ac6fe6594f1515c2d77e44881b2b12619df33
alyviacasey <lesliealyviacasey@gmail.com>
2024-10-11 11:47:23 -0400

    Changes to be committed: 	modified:   classes/inventory.classes.php

--- classes/inventory.classes.php
+++ classes/inventory.classes.php
@@ -24,12 +24,12 @@
                 exit();
             }
 
-            // If statement returns no data... ERROR
+            /*// If statement returns no data... ERROR
             if($stmt->rowCount() == 0) {
                 $stmt = null;
                 header("location: ../inventory.php?error=profilenotfound");
                 exit();
-            }
+            }*/
 
             // NO ERRORS
 
