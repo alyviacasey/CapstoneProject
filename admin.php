323565490b978fd61a6f0c700312e35ed0619446
alyviacasey <lesliealyviacasey@gmail.com>
2024-10-30 17:18:59 -0400

    Changes to be committed: 	modified:   admin.php

--- admin.php
+++ admin.php
@@ -33,22 +33,24 @@
             <h2>ADMIN PANEL</h2>
             <br>
             <div class = "admin-model">
+                <h2>Toy Model Creator</h2>
+                <br>
                 <form action = "includes/model.inc.php" method = "post">
-                    <h3>Theme</h3>
+                    <h4>Theme</h4>
                     <p>Choose an adjective to describe the toy model</p>
                     <input type = "text" name = "theme" rows="10" cols="30" placeholder = "Birthday"> <br>
                     <br>
-                    <h3>Model Name</h3>
+                    <h4>Model Name</h4>
                     <p>Choose an noun to describe the toy model</p>
                     <input type = "text" name = "model" placeholder = "Bear"> <br>
                     <br>
-                    <h3>Model Material</h3>
+                    <h4>Model Material</h4>
                     <input type="radio" id="plush" name="material" value="Plush">
                     <label for="plush">Plush</label> &nbsp; &nbsp;
                     <input type="radio" id="plastic" name="material" value="Plastic">
                     <label for="plastic">Plastic</label> <br>
                     <br>
-                    <h3>Rarity</h3>
+                    <h4>Rarity</h4>
                     <p>Select a rarity for this pet</p>
                     <input type="hidden" id="rarity" name="rarity" value="0">
                     <div class = "stars">
