1c66072c4d8335d24d4a261011eb729d7f3c1f68
alyviacasey <lesliealyviacasey@gmail.com>
2024-11-01 13:26:26 -0400

    Changes to be committed: 	modified:   store.php

--- store.php
+++ store.php
@@ -31,7 +31,7 @@
         <section class = "store">
         <?php foreach($store as $box): ?>
             <div class = "store-box">
-                <img src = "<?= $allboxes->fetchImg($box['model_id']); ?>" alt = "box image">
+                <img src = "<?= $allboxes->fetchImg($box['model_id']) ?>" alt = "box image">
                 <h4><?= $box['name']; ?></h4>
                 <p><?php echo $box['price']; ?></p>
                 <form action = "includes/store.inc.php" method = "post">
