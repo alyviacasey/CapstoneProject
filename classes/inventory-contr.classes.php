dd4b123ee824f7964c5d6caf9a58125177165527
alyviacasey <lesliealyviacasey@gmail.com>
2024-11-01 12:46:24 -0400

    Changes to be committed: 	modified:   classes/inventory-contr.classes.php

--- classes/inventory-contr.classes.php
+++ classes/inventory-contr.classes.php
@@ -69,6 +69,7 @@ class InventoryContr extends Inventory {
         else {
             $this->updateBalance($inventory[0]['balance'] - $price, $this->uid);
             $this->setBox($bid, $this->uid);
+            header("location: ../store.php?error=none");
         }
     }
 
