bf85107674bad7028cdf1a429010b43d26181b9f
alyviacasey <lesliealyviacasey@gmail.com>
2024-11-20 19:06:07 -0500

    Changes to be committed: 	modified:   games/rps.php

--- games/rps.php
+++ games/rps.php
@@ -14,13 +14,15 @@
 </head>
 
 <body>
-    <h3>Rock, Paper, Scissors</h3>
-    <div style="height:300px; width:300px; background-color:white"></div>
-    <form action="games-inc.php" method="post">
-        <input type="radio" id="rock" name="user-choice" value="Rock" /><label for="rock">Rock</label> &nbsp; &nbsp;
-        <input type="radio" id="paper" name="user-choice" value="Paper" /><label for="paper">Paper</label> &nbsp; &nbsp;
-        <input type="radio" id="scissors" name="user-choice" value="Scissors" /><label for="scissors">Scissors</label> &nbsp; &nbsp;
-        <button type = "submit" name = "shoot">SHOOT</button>
-    </form>
+    <div style="padding: 3vw">
+        <h3 style="text-align:center;">Rock, Paper, Scissors</h3>
+        <div style="height:300px; width:300px; background-color:white"></div>
+        <form action="games-inc.php" method="post" style="margin-right: auto; margin-left: auto;">
+            <input type="radio" id="rock" name="user-choice" value="Rock" /><label for="rock">Rock</label> &nbsp; &nbsp;
+            <input type="radio" id="paper" name="user-choice" value="Paper" /><label for="paper">Paper</label> &nbsp; &nbsp;
+            <input type="radio" id="scissors" name="user-choice" value="Scissors" /><label for="scissors">Scissors</label> &nbsp; &nbsp;
+            <button type = "submit" name = "shoot">SHOOT</button>
+        </form>
+    </div>
 </body>
 </html>
