b515934f35659f79d357f531fd20cd00b43da151
alyviacasey <lesliealyviacasey@gmail.com>
2024-11-20 22:30:39 -0500

    Changes to be committed: 	modified:   scripts/rps.js

--- scripts/rps.js
+++ scripts/rps.js
@@ -57,3 +57,10 @@ function declareWinner($userChoice, $computerChoice) {
         return "You lose!";
     }
 }
+
+function rand(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+  
