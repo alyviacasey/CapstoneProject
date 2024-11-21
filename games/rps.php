84fc455a48ce525f91d1f02ab68bb3486956b200
alyviacasey <lesliealyviacasey@gmail.com>
2024-11-20 22:35:31 -0500

    Changes to be committed: 	modified:   games/rps.php

--- games/rps.php
+++ games/rps.php
@@ -11,7 +11,7 @@
     <title>Toybox</title>
     <link rel="stylesheet" href="/css/style.css"> 
     <link rel="stylesheet" href="/css/reset.css"> 
-    <script src="scripts/rps.js" defer></script>
+    <script src="scripts/rps.js"></script>
 </head>
 
 <body>
