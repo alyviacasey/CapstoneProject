294b3a8d77b21905d51cf5535d977957ef79b920
alyviacasey <lesliealyviacasey@gmail.com>
2024-11-22 10:23:35 -0500

    Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <div class = "wrapper">
         <img src="../images/giftboxes/default.png" alt="box" style="width: 200px; height: 200px;"> <br>
         <h2>Hello, welcome to ToyBox!</h2>
-        <p>This is a pet collection webgame where you can play games, earn coins, and spend them to acquire Boxes. </p>
+        <p>This is a pet collection webgame where you can play games, earn coins, and spend them to acquire Boxes, which will give a randomized toy upon opening. Some toys are rarer than others, and may be more difficult to acquire. There are many new friends to meet, so join us!</p>
     </div>
 </body>
 </html>
