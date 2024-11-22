347a8ed69e1fb1f4b6b8edec4d1de1160eed87f2
alyviacasey <lesliealyviacasey@gmail.com>
2024-11-21 22:24:51 -0500

    Changes to be committed: 	modified:   includes/games-rps.inc.php

--- includes/games-rps.inc.php
+++ includes/games-rps.inc.php
@@ -17,5 +17,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $newBalance = $currBalance + $score;
     $inventoryContr->editBalance($newBalance);
-    header("location: ../rps.php");
+    header("location: ../games/rps.php");
 }
